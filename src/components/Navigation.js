69a65c6b98369f596bdb89803d05170739de2afc
danielcwq <dancwq2@gmail.com>
2021-07-22 07:02:59 +0800

    Finished styling for mobile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,20 +7,20 @@ import {Link} from 'react-router-dom'
 export default function Navigation(){
     return(
         <Fade>
-                <div className = 'flex items-center  fixed z-10  w-full mx-auto bg-gray-200'>
+                <div className = 'flex items-center fixed z-10 w-full mx-auto bg-gray-200'>
                     <Link 
                     to = '/'
-                    className = 'text-3xl font-bold text-black items-left ml-10 '>
+                    className = 'text-3xl font-bold text-black items-left mx-auto md:ml-10'>
                     {content.nav.logo}
                     </Link>
 
-                    <div className = 'ml-auto mr-10 border-4 rounded-full border-gray-200 p-2 group hover:shadow-lg hover:border-gray-500'>
+                    <div className = 'mx-4 border-2  md:border-4 rounded-full border-gray-200 p-2 group hover:shadow-lg hover:border-gray-500 md:max-w-xl'>
                         <Link to = "/blog" className ='text-xl text-black'>
                             {content.nav.blogText}
                         </Link>
                         
                     </div>
-                    <div className = 'mr-10 border-4 rounded-full border-gray-200 p-2 group hover:shadow-lg hover:border-gray-500'>
+                    <div className = 'mx-4 border-2 md:border-4 md:mr-10 rounded-full border-gray-200 p-2 group hover:shadow-lg hover:border-gray-500'>
                             <Link 
                             className = 'text-black text-xl'
                             to = "/contact"
