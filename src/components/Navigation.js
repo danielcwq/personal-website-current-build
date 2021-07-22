974ac074bfdc15d14c98e072a74a7c5ef4f66e05
danielcwq <dancwq2@gmail.com>
2021-07-22 09:57:54 +0800

    Fixing styling for mobile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,19 +10,19 @@ export default function Navigation(){
                 <div className = 'flex items-center fixed z-10 w-full mx-auto bg-gray-200'>
                     <Link 
                     to = '/'
-                    className = 'text-3xl font-bold text-black items-left mx-auto md:ml-10'>
+                    className = 'text-2xl md:text-3xl font-bold text-black items-left mx-auto md:ml-10 ml-3'>
                     {content.nav.logo}
                     </Link>
 
                     <div className = 'mx-4 border-2  md:border-4 rounded-full border-gray-200 p-2 group hover:shadow-lg hover:border-gray-500 md:max-w-xl'>
-                        <Link to = "/blog" className ='text-xl text-black'>
+                        <Link to = "/blog" className ='text-lg md:text-xl text-black'>
                             {content.nav.blogText}
                         </Link>
                         
                     </div>
-                    <div className = 'mx-4 border-2 md:border-4 md:mr-10 rounded-full border-gray-200 p-2 group hover:shadow-lg hover:border-gray-500'>
+                    <div className = 'ml-4 border-2 md:border-4 md:mr-10 rounded-full border-gray-200 p-2 group hover:shadow-lg hover:border-gray-500'>
                             <Link 
-                            className = 'text-black text-xl'
+                            className = 'text-lg text-black md:text-xl'
                             to = "/contact"
                             >
                                 {content.nav.contactText}
--- src/components/blogchooser.js
+++ src/components/blogchooser.js
@@ -43,7 +43,7 @@ function blogChoice(){
             This method works, because it forces reviewed content to be brought up consistently, therefore allowing whatever content that is being reviewed to be stored in long-term memory. 
     An example would be starting review two to three months before a major examination, from minute amounts. The key to this is consistency, and having literally no days off. </h2>
             <LazyLoadImage
-            className = 'py-1 px-4 md:px-40'
+            className = 'h-full w-full md:py-1 md:px-4 md:px-40'
             src = {content.blog1.img}/>
             <div className = 'flex justify-center'>
                 <h3 className = 'md:py-1'> Anki Consistently. </h3>
