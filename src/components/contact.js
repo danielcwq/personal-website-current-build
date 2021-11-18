ed9ab685e03d9bec69c3c565279fd4e5728e80d9
Daniel Ching <danielching@Daniels-MacBook-Air.local>
2021-11-18 21:13:44 +0800

    Added styling changes, and substack embed

--- src/components/contact.js
+++ src/components/contact.js
@@ -41,9 +41,8 @@ export default function Contact() {
                  
                   
                 </div>
-                <div className = 'ml-16 md:text-lg text-sm'>
-                <a href = 'https://danielching.substack.com/p/coming-soon?r=tmemp&utm_campaign=post&utm_medium=web&utm_source=' class = 'button' className = "text-black md:text-2xl bg-gray-500 hover:bg-gray-700 border-8 border-gray-500 md:mt-8" target = "_blank"> Substack Subscription</a>
-     
+                <div className = 'flex justify-center'>
+                  <iframe src="https://danielching.substack.com/embed" width="480" height="320" className = "border solid w-4/5 md:w-full rounded-xl md:rounded-2xl" frameborder="0" scrolling="no"></iframe>
                 </div>
            </div>
 
