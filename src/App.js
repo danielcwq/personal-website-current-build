0544250e592dbb6e67991fe7c3e79a38ca4e7945
Daniel Ching <dancwq2@gmail.com>
2023-02-10 20:00:09 +0800

    Trialing GA4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Navigation from './components/Navigation'
 import Header from './components/Header'
 import Stack from './components/Stack'
@@ -13,7 +13,7 @@ import Running from './components/running';
 import DeepLife from './components/deeplife';
 import DeepWork from './components/deepwork';
 import Portfolio from './components/portfolio';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import About from './components/about';
 import RunningRevised from './components/runningRevised';
 import NavigationHome from './components/NavigationHome';
@@ -21,36 +21,36 @@ import ReactGa from 'react-ga';
 
 function App() {
   useEffect(() => {
-    ReactGa.initialize('UA-210213904-1');
+    ReactGa.initialize('G-Y53GCEEX9N');
     // To Report Page View 
     ReactGa.pageview(window.location.pathname + window.location.search);
   }, [])
 
   useEffect(() => {
-   console.log(window.location.pathname)
+    console.log(window.location.pathname)
   })
   return (
     <Router>
       <div>
 
         <Switch>
-          <Route path ='/blog' exact component = {Blog}/>
-          <Route path ='/' exact component = {Home}/>
-          <Route path ='/stack' exact component = {Stack}/>
-          <Route path ='/resume' exact component = {Resume}/>
-          <Route path = '/contact' exact component = {Contactme}/>
-          <Route path='/blog/revision-techniques' exact component={blogChoice}/>
-          <Route path = '/blog/living-the-deep-life' exact component = {LivingDeep}/>
-          <Route path = '/blog/perspectives' exact component = {Perspectives}/>
-          <Route path = '/blog/the-deep-life' exact component = {DeepLife}/>
-          <Route path = '/blog/deep-work' exact component = {DeepWork}/>
-          <Route path = '/portfolio' exact component = {Portfolio}/>
-          <Route path = '/about' exact component = {About}/>
-          <Route path = '/running' exact component = {RunningRevised}/>
+          <Route path='/blog' exact component={Blog} />
+          <Route path='/' exact component={Home} />
+          <Route path='/stack' exact component={Stack} />
+          <Route path='/resume' exact component={Resume} />
+          <Route path='/contact' exact component={Contactme} />
+          <Route path='/blog/revision-techniques' exact component={blogChoice} />
+          <Route path='/blog/living-the-deep-life' exact component={LivingDeep} />
+          <Route path='/blog/perspectives' exact component={Perspectives} />
+          <Route path='/blog/the-deep-life' exact component={DeepLife} />
+          <Route path='/blog/deep-work' exact component={DeepWork} />
+          <Route path='/portfolio' exact component={Portfolio} />
+          <Route path='/about' exact component={About} />
+          <Route path='/running' exact component={RunningRevised} />
         </Switch>
       </div>
     </Router>
-    
+
   );
 }
 
